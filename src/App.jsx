56e056368816fac57a8eff9c8a5323ff43212939
uxrgrtjigtiyrt <rovin2393@naver.com>
2019-11-17 17:22:09 +0900

    q

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import Main from './pages/Main';
-import {createGlobalStyle} from "styled-components";
+import React from "react";
+import Main from "./pages/Main";
+import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle=createGlobalStyle`
-*{
-  margin:0;
-  padding:0;
-}
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-body{
-  background-color:black;
-}
+  body {
+    background-color: black;
+  }
 `;
 
 function App() {
   return (
     <>
-      <GlobalStyle/>
-      <Main/>
+      <GlobalStyle />
+      <Main />
     </>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Banner from "../components/Banner";
 
-class Main extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return <div>
-            <Banner/>
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <Banner />
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
